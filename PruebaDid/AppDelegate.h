5bec71aaef0c90af9afd827bc8139760156a7fd4
Laura Iglesias <laura.iglesias.pina@gmail.com>
2015-05-10 13:22:32 +0200

    Initial Commit

---
+++ PruebaDid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaDid
+//
+//  Created by Laura Iglesias Piña on 10/5/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebaDid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaDid
+//
+//  Created by Laura Iglesias Piña on 10/5/15.
+//  Copyright (c) 2015 lip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
